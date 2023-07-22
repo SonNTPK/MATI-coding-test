2e68e1fbe55ebf8bbd99dcc90b2202b7786537a0
SonNTPK <87081656+SonNTPK@users.noreply.github.com>
2023-07-22 23:33:20 +0700

    Training schedule feature (#1)
    - create page of training schedule
    - create day container component
    - config prettier
    - add common.ts
    
    Co-authored-by: Son(James) <son.nguyen@ucars.sg>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Container, CssBaseline, ThemeProvider } from '@mui/material';
 import './App.css';
+import { theme } from './theme/MUI/custome';
+import TrainingSchedule from './trainingSchedule/page';
 
 function App() {
   return (
-    <div className='App'>
-      <p>Init App</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Container fixed>
+          <div className='app'>
+            <TrainingSchedule></TrainingSchedule>
+          </div>
+        </Container>
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 
